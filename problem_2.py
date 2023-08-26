90f8a3b05ca1f3c15e429e519d033cac1ed90a4f
Lucas1362 <142512916+Lucas1362@users.noreply.github.com>
2023-08-25 22:57:58 -0300

    Update problem_2.py

--- problem_2.py
+++ problem_2.py
@@ -26,8 +26,20 @@ R2 = -2.712
 
 
 def main():
-    pass  # your code here
+    a = float(input("Digite o valor A: "))
+    b = float(input("Digite o valor B: "))
+    c = float(input("Digite o valor C: "))
 
+
+    form_bhaskara = b**2 - 4*a*c
+
+    if a == 0 or form_bhaskara < 0:
+        print("Impossível calcular.")
+    else:
+        r1 = (-b + form_bhaskara**0.5) / (2*a)
+        r2 = (-b - form_bhaskara**0.5) / (2*a)
+        print(f"R1 = {r1:.3f}")
+        print(f"R2 = {r2:.3f}")
 
 if __name__ == '__main__':
     main()
